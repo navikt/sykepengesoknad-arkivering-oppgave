772717db3fe10d011393f93f7a13edacb5318534
Anders Østby <anders.ostby@nav.no>
2018-06-28 19:49:57 +0200

    Konsumerer fra kafkatopic på felles kafkacluster

--- src/main/java/no/nav/syfo/config/KafkaConfig.java
+++ src/main/java/no/nav/syfo/config/KafkaConfig.java
@@ -1,19 +1,12 @@
 package no.nav.syfo.config;
 
-import org.apache.kafka.clients.consumer.ConsumerConfig;
-import org.apache.kafka.clients.producer.ProducerConfig;
-import org.apache.kafka.common.serialization.IntegerDeserializer;
-import org.apache.kafka.common.serialization.IntegerSerializer;
-import org.apache.kafka.common.serialization.StringDeserializer;
-import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
-import org.springframework.kafka.core.*;
-
-import java.util.HashMap;
-import java.util.Map;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
 
 
 @Configuration
@@ -28,19 +21,8 @@ public class KafkaConfig {
     }
 
     @Bean
-    public ConsumerFactory<Integer, String> consumerFactory() {
-        return new DefaultKafkaConsumerFactory<>(consumerConfigs());
-    }
-
-    @Bean
-    public Map<String, Object> consumerConfigs() {
-        Map<String, Object> props = new HashMap<>();
-        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
-        props.put(ConsumerConfig.GROUP_ID_CONFIG, "bar");
-        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, IntegerDeserializer.class.getName());
-        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
-        props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
-        return props;
+    public ConsumerFactory<Integer, String> consumerFactory(KafkaProperties properties) {
+        return new DefaultKafkaConsumerFactory<>(properties.buildConsumerProperties());
     }
 }
 
--- src/main/java/no/nav/syfo/consumer/kafka/kafka/TestListener.java
+++ src/main/java/no/nav/syfo/consumer/kafka/kafka/TestListener.java
@@ -5,11 +5,25 @@ import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+
 @Component
 @Slf4j
 public class TestListener {
-    @KafkaListener(topics = "testtopic")
+    @KafkaListener(topics = "aapen-syfo-soeknadSendt-v1")
     public void listen(ConsumerRecord<Integer, String> cr) throws Exception {
-        log.info("Mottatt melding med offset {}, id {} og value {}", cr.offset(), cr.key(), cr.value());
+        log.info("Mottatt melding med timestamp {} partition {}, offset {}, id {} og value {}",
+                toLocalDateTime(cr.timestamp()).format(DateTimeFormatter.ISO_DATE_TIME),
+                cr.partition(),
+                cr.offset(),
+                cr.key(),
+                cr.value());
+    }
+
+    private LocalDateTime toLocalDateTime(long timestamp) {
+        return Instant.ofEpochMilli(timestamp).atZone(ZoneId.systemDefault()).toLocalDateTime();
     }
 }
