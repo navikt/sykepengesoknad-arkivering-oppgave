56459e6495fdf85879cb7bbff6bbff28eeb84477
Knut Nygaard <knutnyg@gmail.com>
2018-07-31 09:41:44 +0200

    Forhindrer logspam fra atomikos
    # Conflicts:
    #	src/main/java/no/nav/syfo/config/ApplicationConfig.java

--- src/main/java/no/nav/syfo/config/ApplicationConfig.java
+++ src/main/java/no/nav/syfo/config/ApplicationConfig.java
@@ -1,6 +1,10 @@
 package no.nav.syfo.config;
 
+import org.flywaydb.core.Flyway;
+import org.springframework.boot.autoconfigure.flyway.FlywayMigrationStrategy;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.jta.JtaTransactionManager;
 import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
@@ -9,6 +13,12 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EnableTransactionManagement
 @EnableKafka
 public class ApplicationConfig {
+    // Sørger for at flyway migrering skjer etter at JTA transaction manager er ferdig satt opp av Spring.
+    // Forhindrer WARNING: transaction manager not running? loggspam fra Atomikos.
+    @Bean
+    FlywayMigrationStrategy flywayMigrationStrategy(final JtaTransactionManager jtaTransactionManager) {
+        return Flyway::migrate;
+    }
 }
 
 
