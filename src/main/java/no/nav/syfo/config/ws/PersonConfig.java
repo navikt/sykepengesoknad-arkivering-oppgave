a17b74355559d518f62c4ca0b18d7ea9ef94b090
Kim Stephen Bovim <kim.stephen.bovim@nav.no>
2018-08-02 09:29:32 +0200

    setter properties i STSClientConfig og mocker opp WS

--- src/main/java/no/nav/syfo/config/ws/PersonConfig.java
+++ src/main/java/no/nav/syfo/config/ws/PersonConfig.java
@@ -4,16 +4,20 @@ import no.nav.syfo.consumer.util.ws.LogErrorHandler;
 import no.nav.syfo.consumer.util.ws.WsClient;
 import no.nav.tjeneste.virksomhet.person.v3.binding.PersonV3;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 import java.util.Collections;
 
 @Configuration
 public class PersonConfig {
 
-    @Bean
     @SuppressWarnings("unchecked")
+    @Bean
+    @ConditionalOnProperty(value = "mockWS", havingValue = "false", matchIfMissing = true)
+    @Primary
     public PersonV3 personV3(@Value("${virksomhet.person.v3.endpointurl}") String serviceUrl) {
         return new WsClient<PersonV3>().createPort(serviceUrl, PersonV3.class, Collections.singletonList(new LogErrorHandler()));
     }
