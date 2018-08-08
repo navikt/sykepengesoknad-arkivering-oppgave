b6488b174f2c3e4a0c7449d455efd40cb676eff1
Kim Stephen Bovim <kim.stephen.bovim@nav.no>
2018-08-08 09:24:06 +0200

    fikser test

--- src/test/java/no/nav/syfo/kafka/DeserialiseringTest.java
+++ src/test/java/no/nav/syfo/kafka/DeserialiseringTest.java
@@ -1,6 +1,7 @@
 package no.nav.syfo.kafka;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import no.nav.syfo.domain.dto.Sykepengesoknad;
 import org.junit.Test;
 
@@ -12,9 +13,9 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class DeserialiseringTest {
     @Test
     public void deserialiserSoknad() throws IOException {
-        String serialisertSoknad = "{\"id\":\"test-kafka-sykepengesoknad\",\"aktorId\":\"aktorId\",\"sykmeldingId\":\"sykmelding-id\",\"soknadstype\":\"SELVSTENDIGE_OG_FRILANSERE\",\"status\":\"TIL_SENDING\",\"fom\":\"2018-06-06\",\"tom\":\"2018-07-07\",\"opprettetDato\":\"2018-06-06\",\"sporsmal\":[{\"id\":\"1\",\"tag\":null,\"uuid\":null,\"sporsmalstekst\":\"Dette er et testspørsmål\",\"undertekst\":null,\"svartype\":\"PROSENT\",\"min\":null,\"max\":null,\"kriterieForVisningAvUndersporsmal\":null,\"svar\":[{\"svarverdiType\":null,\"verdi\":\"69\"}],\"undersporsmal\":null}],\"innsendtDato\":\"2018-06-20\"}";
+        String serialisertSoknad = "{\"id\":\"test-kafka-sykepengesoknad\",\"aktorId\":\"aktorId\",\"sykmeldingId\":\"sykmelding-id\",\"soknadstype\":\"SELVSTENDIGE_OG_FRILANSERE\",\"status\":\"TIL_SENDING\",\"fom\":[2018,6,6],\"tom\":[2018,7,7],\"opprettetDato\":[2018,6,6],\"sporsmal\":[{\"id\":\"1\",\"tag\":null,\"uuid\":null,\"sporsmalstekst\":\"Dette er et testspørsmål\",\"undertekst\":null,\"svartype\":\"PROSENT\",\"min\":null,\"max\":null,\"kriterieForVisningAvUndersporsmal\":null,\"svar\":[{\"svarverdiType\":null,\"verdi\":\"69\"}],\"undersporsmal\":null}],\"innsendtDato\":[2018,6,20]}";
 
-        Sykepengesoknad sykepengesoknad = new ObjectMapper().readValue(serialisertSoknad, Sykepengesoknad.class);
+        Sykepengesoknad sykepengesoknad = new ObjectMapper().registerModule(new JavaTimeModule()).readValue(serialisertSoknad, Sykepengesoknad.class);
         assertThat(sykepengesoknad.getAktorId()).isEqualTo("aktorId");
         assertThat(sykepengesoknad.getSporsmal().size()).isEqualTo(1);
     }
