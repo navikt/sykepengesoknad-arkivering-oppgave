7cba51c975d753e81d548d661fabdc798fecf0b4
Anders Østby <anders.ostby@nav.no>
2018-08-08 14:05:00 +0200

    Setter WRITE_DATES_AS_TIMESTAMPS = false i RestTemplate

--- src/main/java/no/nav/syfo/controller/PDFRestController.java
+++ src/main/java/no/nav/syfo/controller/PDFRestController.java
@@ -1,33 +1,33 @@
 package no.nav.syfo.controller;
 
+import com.fasterxml.jackson.databind.SerializationFeature;
 import lombok.extern.slf4j.Slf4j;
+import no.nav.syfo.domain.Soknad;
 import no.nav.syfo.domain.dto.PDFTemplate;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import no.nav.syfo.domain.Soknad;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.*;
+import org.springframework.http.converter.json.AbstractJackson2HttpMessageConverter;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.client.RestTemplate;
-
 
 import static org.springframework.http.HttpStatus.OK;
 
 @Slf4j
 @Controller
 public class PDFRestController {
-
-
     private String pdfPath;
+    private RestTemplate restTemplate;
     private static final Logger LOG = LoggerFactory.getLogger(PDFRestController.class);
 
-    public PDFRestController(@Value("${pdfgen.url}") String pdfPath) {
+    public PDFRestController(@Value("${pdfgen.url}") String pdfPath,
+                             RestTemplate restTemplate) {
         this.pdfPath = pdfPath;
+        this.restTemplate = restTemplate;
     }
 
     public byte[] getPDF(Soknad soknad, PDFTemplate template) {
-
-
         final String url = this.pdfPath + "/v1/genpdf/syfosoknader/" + template;
 
         final HttpHeaders headers = new HttpHeaders();
@@ -36,7 +36,7 @@ public class PDFRestController {
         final HttpEntity<Soknad> entity = new HttpEntity<>(soknad, headers);
 
         log.info(entity.toString());
-        final ResponseEntity<byte[]> result = new RestTemplate().exchange(url, HttpMethod.POST, entity, byte[].class);
+        final ResponseEntity<byte[]> result = restTemplate.exchange(url, HttpMethod.POST, entity, byte[].class);
 
         if (result.getStatusCode() != OK) {
             throw new RuntimeException("getPDF feiler med HTTP-" + result.getStatusCode() + " for søknad om utenlandsopphold med id: " + soknad.soknadsId);
