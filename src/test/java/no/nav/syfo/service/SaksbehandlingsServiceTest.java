516b92c1c7ea8f0d6dafbf0ec4e6cdf83bba6664
Martin Solheim <martin.solheim@me.com>
2018-08-13 16:12:54 +0200

    Implementerer test som verifiserer at feil ved innsending fanges

---
+++ src/test/java/no/nav/syfo/service/SaksbehandlingsServiceTest.java
@@ -0,0 +1,61 @@
+package no.nav.syfo.service;
+
+import no.nav.syfo.consumer.repository.InnsendingDAO;
+import no.nav.syfo.consumer.ws.AktørConsumer;
+import no.nav.syfo.consumer.ws.BehandleJournalConsumer;
+import no.nav.syfo.consumer.ws.BehandleSakConsumer;
+import no.nav.syfo.consumer.ws.PersonConsumer;
+import no.nav.syfo.domain.dto.Soknadstype;
+import no.nav.syfo.domain.dto.Sykepengesoknad;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import java.time.LocalDate;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+@RunWith(MockitoJUnitRunner.class)
+public class SaksbehandlingsServiceTest {
+
+    @Mock
+    private AktørConsumer aktørConsumer;
+    @Mock
+    private PersonConsumer personConsumer;
+    @Mock
+    private InnsendingDAO innsendingDAO;
+    @Mock
+    private BehandleSakConsumer behandleSakConsumer;
+    @Mock
+    private BehandleJournalConsumer behandleJournalConsumer;
+
+    @InjectMocks
+    SaksbehandlingsService saksbehandlingsService;
+
+    @Test
+    public void handtereBehandleSoknad() {
+        when(aktørConsumer.finnFnr(any())).thenReturn("12345678901");
+        when(personConsumer.finnBrukerPersonnavnByFnr(any())).thenReturn("Personnavn");
+        when(innsendingDAO.opprettInnsending(any(), any())).thenReturn("uuid");
+        when(behandleSakConsumer.opprettSak(any())).thenReturn("saksId");
+        when(behandleJournalConsumer.opprettJournalpost(any(), any())).thenThrow(new RuntimeException("Opprett journal feilet"));
+
+        Sykepengesoknad sykepengesoknad = new Sykepengesoknad();
+        sykepengesoknad.setId("id");
+        sykepengesoknad.setSykmeldingId("sykmeldingId");
+        sykepengesoknad.setAktorId("aktorId");
+        sykepengesoknad.setSoknadstype(Soknadstype.SELVSTENDIGE_OG_FRILANSERE);
+        sykepengesoknad.setStatus("status");
+        sykepengesoknad.setFom(LocalDate.now());
+        sykepengesoknad.setTom(LocalDate.now());
+        sykepengesoknad.setOpprettetDato(LocalDate.now());
+        sykepengesoknad.setInnsendtDato(LocalDate.now());
+
+        saksbehandlingsService.behandleSoknad(sykepengesoknad);
+
+        verify(innsendingDAO, times(1)).leggTilFeiletInnsending(any());
+    }
+}
