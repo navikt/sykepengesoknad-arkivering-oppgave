9cf4c37521fb1afc50aa251024248841c1e75b99
Knut Nygaard <knutnyg@gmail.com>
2018-08-15 10:24:36 +0200

    Reworket toggle for å skru av lesing fra kø
    Nå skrur vi av selve lesingen, fremfor å bare la være å prosessere meldinger.

--- src/main/java/no/nav/syfo/config/ApplicationConfig.java
+++ src/main/java/no/nav/syfo/config/ApplicationConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.json.AbstractJackson2HttpMessageConverter;
 import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.transaction.jta.JtaTransactionManager;
 import org.springframework.web.client.RestTemplate;
@@ -15,6 +16,7 @@ import org.springframework.web.client.RestTemplate;
 @Configuration
 @EnableTransactionManagement
 @EnableKafka
+@EnableScheduling
 public class ApplicationConfig {
     // Sørger for at flyway migrering skjer etter at JTA transaction manager er ferdig satt opp av Spring.
     // Forhindrer WARNING: transaction manager not running? loggspam fra Atomikos.
--- src/main/java/no/nav/syfo/kafka/SoknadSendtListener.java
+++ src/main/java/no/nav/syfo/kafka/SoknadSendtListener.java
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import lombok.extern.slf4j.Slf4j;
-import no.nav.syfo.config.unleash.Toggle;
 import no.nav.syfo.domain.dto.Sykepengesoknad;
 import no.nav.syfo.service.SaksbehandlingsService;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
@@ -17,22 +16,18 @@ import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
 
-import static no.nav.syfo.config.unleash.FeatureToggle.SKAL_LESE_SOKNADER_FRA_KOE;
-
 @Component
 @Slf4j
 public class SoknadSendtListener {
 
     private final SaksbehandlingsService saksbehandlingsService;
-    private final Toggle toggle;
 
     @Inject
-    public SoknadSendtListener(SaksbehandlingsService saksbehandlingsService, Toggle toggle) {
+    public SoknadSendtListener(SaksbehandlingsService saksbehandlingsService) {
         this.saksbehandlingsService = saksbehandlingsService;
-        this.toggle = toggle;
     }
 
-    @KafkaListener(topics = "aapen-syfo-soeknadSendt-v1")
+    @KafkaListener(topics = "aapen-syfo-soeknadSendt-v1", id = "soknadSendt")
     public void listen(ConsumerRecord<String, String> cr) throws Exception {
         log.info("Mottatt melding med timestamp {} partition {}, offset {}, id {} og value {}",
                 toLocalDateTime(cr.timestamp()).format(DateTimeFormatter.ISO_DATE_TIME),
@@ -41,17 +36,13 @@ public class SoknadSendtListener {
                 cr.key(),
                 cr.value());
 
-        if (toggle.isEnabled(SKAL_LESE_SOKNADER_FRA_KOE)) {
-            try {
-                Sykepengesoknad deserialisertSoknad = new ObjectMapper().registerModule(new JavaTimeModule()).readValue(cr.value(), Sykepengesoknad.class);
-                log.info("Deserialiserte sykepengesøknad: {}", deserialisertSoknad.toString());
+        try {
+            Sykepengesoknad deserialisertSoknad = new ObjectMapper().registerModule(new JavaTimeModule()).readValue(cr.value(), Sykepengesoknad.class);
+            log.info("Deserialiserte sykepengesøknad: {}", deserialisertSoknad.toString());
 
-                saksbehandlingsService.behandleSoknad(deserialisertSoknad);
-            } catch (JsonProcessingException e) {
-                log.error("Kunne ikke deserialisere sykepengesøknad", e);
-            }
-        } else {
-            log.info("Togglet av: Skipper saksbehandling");
+            saksbehandlingsService.behandleSoknad(deserialisertSoknad);
+        } catch (JsonProcessingException e) {
+            log.error("Kunne ikke deserialisere sykepengesøknad", e);
         }
     }
 
