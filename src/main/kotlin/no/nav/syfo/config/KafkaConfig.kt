a84849cba9869047a51d650fd88d59d8d05faa8f
bkulsveen <bjorn.inge.kulsveen@nav.no>
2019-09-19 13:30:42 +0200

    Opprettet dezerialization funkjson for arbeidsledig consumer topic

--- src/main/kotlin/no/nav/syfo/config/KafkaConfig.kt
+++ src/main/kotlin/no/nav/syfo/config/KafkaConfig.kt
@@ -8,6 +8,7 @@ import no.nav.syfo.kafka.interfaces.Soknad
 import no.nav.syfo.kafka.soknad.deserializer.MultiFunctionDeserializer
 import no.nav.syfo.kafka.soknad.dto.SoknadDTO
 import no.nav.syfo.kafka.sykepengesoknad.dto.SykepengesoknadDTO
+import no.nav.syfo.kafka.sykepengesoknadarbeidsledig.dto.SykepengesoknadArbeidsledigDTO
 import org.apache.kafka.common.serialization.StringDeserializer
 import org.springframework.boot.autoconfigure.kafka.KafkaProperties
 import org.springframework.context.annotation.Bean
@@ -17,7 +18,7 @@ import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory
 import org.springframework.kafka.core.ConsumerFactory
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory
 import org.springframework.kafka.listener.AbstractMessageListenerContainer
-
+import java.io.IOException
 import java.util.function.BiFunction
 
 @Configuration
@@ -47,6 +48,35 @@ class KafkaConfig {
             MultiFunctionDeserializer(mapOf(
                     "SYKEPENGESOKNAD" to BiFunction { _, bytes -> objectMapper.readValue(bytes, SykepengesoknadDTO::class.java) },
                     "SOKNAD" to BiFunction { _, bytes -> objectMapper.readValue(bytes, SoknadDTO::class.java) })))
+    }
+
+    @Bean
+    fun kafkaListenerContainerFactoryArbeidsledig(
+        consumerFactory: ConsumerFactory<String, SykepengesoknadArbeidsledigDTO>,
+        kafkaErrorHandler: KafkaErrorHandler): ConcurrentKafkaListenerContainerFactory<String, SykepengesoknadArbeidsledigDTO> {
+        val factory = ConcurrentKafkaListenerContainerFactory<String, SykepengesoknadArbeidsledigDTO>()
+        factory.containerProperties.ackMode = AbstractMessageListenerContainer.AckMode.MANUAL_IMMEDIATE
+        factory.containerProperties.setErrorHandler(kafkaErrorHandler)
+        factory.consumerFactory = consumerFactory
+        return factory
+    }
+
+    @Bean
+    fun consumerFactoryArbeidsledig(properties: KafkaProperties): ConsumerFactory<String, SykepengesoknadArbeidsledigDTO>  {
+        val objectMapper = ObjectMapper()
+            .registerModule(JavaTimeModule())
+            .configure(DeserializationFeature.READ_UNKNOWN_ENUM_VALUES_USING_DEFAULT_VALUE, true)
+        return DefaultKafkaConsumerFactory(
+            properties.buildConsumerProperties(),
+            StringDeserializer(),
+            MultiFunctionDeserializer(emptyMap()
+            ) { bytes ->
+                try {
+                    objectMapper.readValue<SykepengesoknadArbeidsledigDTO>(bytes, SykepengesoknadArbeidsledigDTO::class.java)
+                } catch (e: IOException) {
+                    throw RuntimeException("Feil ved konvertering av bytes til SykepengesoknadArbeidsledigDTO", e)
+                }
+            })
     }
 }
 
