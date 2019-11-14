7622006a7ce361f5021faf5d108eaee043f0f4da
Martin Solheim <martin.solheim@me.com>
2019-11-14 20:09:37 +0100

    Legger til flyway

--- src/main/kotlin/no/nav/syfo/Application.kt
+++ src/main/kotlin/no/nav/syfo/Application.kt
@@ -2,12 +2,13 @@ package no.nav.syfo
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.flyway.FlywayAutoConfiguration
 import org.springframework.transaction.annotation.EnableTransactionManagement
 
 const val AZUREAD = "azuread"
 const val BEHANDLINGSTIDSPUNKT = "behandlingstidspunkt"
 
-@SpringBootApplication
+@SpringBootApplication(exclude = [FlywayAutoConfiguration::class])
 @EnableTransactionManagement
 class Application {
     companion object {
