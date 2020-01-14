dbcb0ffe2812b77f9aac280ff785322ddf5ff7b5
kulsveen <bjorn.inge.kulsveen@nav.no>
2020-01-14 15:18:06 +0100

    Naiserator

--- src/main/kotlin/no/nav/syfo/config/WebMVCConfig.kt
+++ src/main/kotlin/no/nav/syfo/config/WebMVCConfig.kt
@@ -9,9 +9,9 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @Configuration
 class WebMVCConfig(
-        @Value("\${aad_syfoinntektsmelding_clientid_username}") val syfoinntektsmeldingClientId:String,
-        @Value("\${no.nav.security.oidc.issuer.azuread.acceptedaudience}") val syfogsakClientId:String,
-        @Value("\${syfo-jwt-issuer.url}") val issuer:String) : WebMvcConfigurer {
+        @Value("\${aad_syfoinntektsmelding_clientid_username}") val syfoinntektsmeldingClientId: String,
+        @Value("\${no.nav.security.oidc.issuer.azuread.acceptedaudience}") val syfogsakClientId: String,
+        @Value("\${syfo.jwt.issuer.url}") val issuer: String) : WebMvcConfigurer {
 
     override fun addInterceptors(registry: InterceptorRegistry) {
         registry.addInterceptor(AzureADTokenFilter(syfogsakClientId, listOf(syfoinntektsmeldingClientId), issuer))
