5f4fcdcd376b6fc8596a40eca525ba6c1250ad92
golshidbahadorian <golshid.bahadorian@nav.no>
2022-02-14 14:55:26 +0100

    Merge

--- src/main/kotlin/no/nav/syfo/service/BehandleVedTimeoutService.kt
+++ src/main/kotlin/no/nav/syfo/service/BehandleVedTimeoutService.kt
@@ -9,7 +9,6 @@ import no.nav.syfo.kafka.mapper.toSykepengesoknad
 import no.nav.syfo.logger
 import no.nav.syfo.repository.OppgaveRepository
 import no.nav.syfo.repository.OppgaveStatus
-import no.nav.syfo.repository.OppgavestyringDAO
 import org.springframework.context.annotation.Profile
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Component
