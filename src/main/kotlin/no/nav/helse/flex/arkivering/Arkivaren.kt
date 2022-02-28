b9bb2d6a1bc6ef34c77005434d18c00e17cb47c2
Håvard Stigen Andersen <havard.stigen.andersen@nav.no>
2022-02-28 14:55:11 +0100

    Måler tid (#26)

--- src/main/kotlin/no/nav/helse/flex/arkivering/Arkivaren.kt
+++ src/main/kotlin/no/nav/helse/flex/arkivering/Arkivaren.kt
@@ -4,7 +4,6 @@ import io.micrometer.core.instrument.MeterRegistry
 import no.nav.helse.flex.client.DokArkivClient
 import no.nav.helse.flex.client.PDFClient
 import no.nav.helse.flex.domain.JournalpostRequest
-import no.nav.helse.flex.domain.JournalpostResponse
 import no.nav.helse.flex.domain.Soknad
 import no.nav.helse.flex.domain.dto.PDFTemplate
 import no.nav.helse.flex.domain.dto.Soknadstype
@@ -22,19 +21,23 @@ class Arkivaren(
 
     fun opprettJournalpost(soknad: Soknad): String {
 
-        val soknadsPDF: ByteArray = pdfClient.getPDF(soknad = soknad, template = hentPDFTemplateEtterSoknadstype(soknad.soknadstype))
-
-        val request: JournalpostRequest =
-            skapJournalpostRequest(pdf = soknadsPDF, soknad = soknad)
-        val journalpostResponse: JournalpostResponse = dokArkivClient.opprettJournalpost(request, soknad.soknadsId!!)
-
-        if (!journalpostResponse.journalpostferdigstilt) {
-            log.warn("Journalpost ${journalpostResponse.journalpostId} for søknad ${soknad.soknadsId} ble ikke ferdigstilt")
+        val pdf = measureTimeMillisWithResult {
+            pdfClient.getPDF(soknad = soknad, template = hentPDFTemplateEtterSoknadstype(soknad.soknadstype))
         }
 
-        log.info("Arkiverte søknad ${soknad.soknadsId}")
+        val request: JournalpostRequest =
+            skapJournalpostRequest(pdf = pdf.result, soknad = soknad)
+
+        val journalpostResponse = measureTimeMillisWithResult {
+            dokArkivClient.opprettJournalpost(request, soknad.soknadsId!!)
+        }
+        if (!journalpostResponse.result.journalpostferdigstilt) {
+            log.warn("Journalpost ${journalpostResponse.result.journalpostId} for søknad ${soknad.soknadsId} ble ikke ferdigstilt")
+        }
+
+        log.info("Arkiverte søknad ${soknad.soknadsId}. PDF tid: ${pdf.millis} . Dokarkiv tid: ${journalpostResponse.millis}")
         registry.counter("søknad_arkivert").increment()
-        return journalpostResponse.journalpostId
+        return journalpostResponse.result.journalpostId
     }
 
     private fun hentPDFTemplateEtterSoknadstype(soknadstype: Soknadstype): PDFTemplate {
@@ -49,4 +52,18 @@ class Arkivaren(
             Soknadstype.GRADERT_REISETILSKUDD -> PDFTemplate.GRADERT_REISETILSKUDD
         }
     }
+}
+
+class MeassureBlock<T>(
+    val millis: Long,
+    val result: T,
+)
+
+inline fun <T> measureTimeMillisWithResult(block: () -> T): MeassureBlock<T> {
+    val start = System.currentTimeMillis()
+    val result = block()
+    return MeassureBlock(
+        millis = System.currentTimeMillis() - start,
+        result = result
+    )
 }
