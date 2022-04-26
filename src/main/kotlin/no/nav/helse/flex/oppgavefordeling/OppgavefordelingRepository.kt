20a4c9bbd2b00b9d693608010f916c5e690f327e
kulsveen <bjorn.inge.kulsveen@nav.no>
2022-04-26 14:23:17 +0200

    Datbasespørring

---
+++ src/main/kotlin/no/nav/helse/flex/oppgavefordeling/OppgavefordelingRepository.kt
@@ -0,0 +1,36 @@
+package no.nav.helse.flex.oppgavefordeling
+
+import org.springframework.data.annotation.Id
+import org.springframework.data.jdbc.repository.query.Modifying
+import org.springframework.data.jdbc.repository.query.Query
+import org.springframework.data.relational.core.mapping.Table
+import org.springframework.data.repository.CrudRepository
+import org.springframework.stereotype.Repository
+import java.time.Instant
+
+@Repository
+interface OppgavefordelingRepository : CrudRepository<OppgavefordelingDbRecord, String> {
+    fun findBySykepengesoknadId(sykepengesoknadId: String): OppgavefordelingDbRecord?
+
+    @Modifying
+    @Query("""INSERT INTO oppgavefordeling(sykepengesoknad_id, status) VALUES (:sykepengesoknadId, :status)""")
+    fun insert(sykepengesoknadId: String, status: OppgavefordelingStatus)
+
+    @Modifying
+    @Query("""UPDATE oppgavefordeling SET avstemt = TRUE, sendt_nav = :sendtNav WHERE sykepengesoknad_id = :sykepengesoknadId""")
+    fun settTilAvstemt(sykepengesoknadId: String, sendtNav: Instant): Boolean
+}
+
+@Table("oppgavefordeling")
+data class OppgavefordelingDbRecord(
+    @Id
+    val sykepengesoknadId: String,
+    val status: OppgavefordelingStatus,
+    val timeout: Instant? = null,
+    val avstemt: Boolean = false,
+    val sendtNav: Instant? = null,
+)
+
+enum class OppgavefordelingStatus {
+    LagOppgave, LagOppgaveForSpeilsaksbehandlere
+}
