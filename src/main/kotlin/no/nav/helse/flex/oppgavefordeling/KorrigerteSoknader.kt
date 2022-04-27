3d33f5bd470c01423b57b8b6203138af515bc68a
Nils Jørgen Mittet <nils.jorgen.mittet@nav.no>
2022-04-27 15:38:00 +0200

    Lint koden

--- src/main/kotlin/no/nav/helse/flex/oppgavefordeling/KorrigerteSoknader.kt
+++ src/main/kotlin/no/nav/helse/flex/oppgavefordeling/KorrigerteSoknader.kt
@@ -28,9 +28,9 @@ class KorrigerteSoknader(
     fun listenBatch(cr: List<ConsumerRecord<String, String>>, acknowledgment: Acknowledgment) {
         cr.forEach {
             val soknad = it.value().tilEnkelSoknad()
-            if (soknad.status == Soknadstatus.KORRIGERT
-                && soknad.soknadstype == Soknadstype.ARBEIDSTAKERE
-                && soknad.korrigertAv != null
+            if (soknad.status == Soknadstatus.KORRIGERT &&
+                soknad.soknadstype == Soknadstype.ARBEIDSTAKERE &&
+                soknad.korrigertAv != null
             ) {
                 oppgavefordelingRepository.settkorrigertAv(soknad.korrigertAv, soknad.id)
             }
