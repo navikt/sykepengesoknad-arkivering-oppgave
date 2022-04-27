ff63e94120b42957caeb5dff0688e2771173ff66
Nils Jørgen Mittet <nils.jorgen.mittet@nav.no>
2022-04-27 14:19:07 +0200

    Sett Kafka reset policy

--- src/main/kotlin/no/nav/helse/flex/kafka/AivenKafkaConfig.kt
+++ src/main/kotlin/no/nav/helse/flex/kafka/AivenKafkaConfig.kt
@@ -64,6 +64,7 @@ class AivenKafkaConfig(
     ): ConcurrentKafkaListenerContainerFactory<String, String> {
         val config = mapOf(
             ConsumerConfig.GROUP_ID_CONFIG to "syfogsak-consumer",
+            ConsumerConfig.AUTO_OFFSET_RESET_CONFIG to "latest",
             ConsumerConfig.AUTO_OFFSET_RESET_CONFIG to kafkaAutoOffsetReset,
             ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG to false,
             ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG to StringDeserializer::class.java,
