847f7fc366f6fb3ec1b5707bad98d7ae989b0ef9
kulsveen <bjorn.inge.kulsveen@nav.no>
2022-04-29 17:36:52 +0200

    Mindre last på databasen

--- src/main/kotlin/no/nav/helse/flex/oppgavefordeling/OppgavefordelingBatchRepository.kt
+++ src/main/kotlin/no/nav/helse/flex/oppgavefordeling/OppgavefordelingBatchRepository.kt
@@ -3,23 +3,11 @@ package no.nav.helse.flex.oppgavefordeling
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate
 import org.springframework.jdbc.core.namedparam.SqlParameterSourceUtils
 import org.springframework.stereotype.Repository
-import java.time.OffsetDateTime
 
 @Repository
 class OppgavefordelingBatchRepository(
     private val namedParameterJdbcTemplate: NamedParameterJdbcTemplate,
 ) {
-
-    fun settSendtTilArbeidsGiver(arbeidsgiverDatoer: List<ArbeidsgiverDato>) {
-        val sql = """
-            UPDATE oppgavefordeling 
-            SET sendt_arbeidsgiver = :sendtArbeidsgiver 
-            WHERE sykepengesoknad_id = :sykepengesoknadId
-            """
-
-        val batchParams = SqlParameterSourceUtils.createBatch(arbeidsgiverDatoer)
-        namedParameterJdbcTemplate.batchUpdate(sql, batchParams)
-    }
 
     fun settFodselsnummer(fodselsnummere: List<Fodselsnummer>) {
         val sql = """
@@ -32,11 +20,6 @@ class OppgavefordelingBatchRepository(
         namedParameterJdbcTemplate.batchUpdate(sql, batchParams)
     }
 }
-
-data class ArbeidsgiverDato(
-    val sykepengesoknadId: String,
-    val sendtArbeidsgiver: OffsetDateTime
-)
 
 data class Fodselsnummer(
     val sykepengesoknadId: String,
