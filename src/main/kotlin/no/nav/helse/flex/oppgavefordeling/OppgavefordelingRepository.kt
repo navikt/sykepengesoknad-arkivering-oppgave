6864b25e0523b2e415c103d98122143b9d9172af
kulsveen <bjorn.inge.kulsveen@nav.no>
2022-05-02 17:58:36 +0200

    Ferdig med henting av geografisk tilknytning

--- src/main/kotlin/no/nav/helse/flex/oppgavefordeling/OppgavefordelingRepository.kt
+++ src/main/kotlin/no/nav/helse/flex/oppgavefordeling/OppgavefordelingRepository.kt
@@ -15,33 +15,6 @@ interface OppgavefordelingRepository : CrudRepository<OppgavefordelingDbRecord, 
     @Modifying
     @Query("""INSERT INTO oppgavefordeling(sykepengesoknad_id, status) VALUES (:sykepengesoknadId, :status)""")
     fun insert(sykepengesoknadId: String, status: OppgavefordelingStatus)
-
-    @Query(
-        """
-        SELECT *
-        FROM oppgavefordeling a
-          INNER JOIN innsending b ON b.sykepengesoknad_id = a.sykepengesoknad_id
-        WHERE b.oppgave_id IS NULL
-          AND a.avstemt IS TRUE
-          AND a.kommune IS NULL 
-          AND a.bydel IS NULL 
-          AND a.land IS NULL 
-        LIMIT 100
-        """
-    )
-    fun hent100UtenGT(): List<OppgavefordelingDbRecord>
-
-    @Modifying
-    @Query(
-        """
-        UPDATE oppgavefordeling 
-        SET kommune = :kommune,
-            bydel = :bydel,
-            land = :land
-        WHERE sykepengesoknad_id = :sykepengesoknadId
-        """
-    )
-    fun lagreGeografiskTilknytning(sykepengesoknadId: String, kommune: String?, bydel: String?, land: String?)
 }
 
 @Table("oppgavefordeling")
