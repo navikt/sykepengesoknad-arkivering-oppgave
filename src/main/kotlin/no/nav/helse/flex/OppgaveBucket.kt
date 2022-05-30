c296518a2d75249dbdc4d67f2e0c952d8e8b220d
kulsveen <bjorn.inge.kulsveen@nav.no>
2022-05-30 15:21:02 +0200

    Henter soknad i fra sykepengesoknad-backend

--- src/main/kotlin/no/nav/helse/flex/OppgaveBucket.kt
+++ src/main/kotlin/no/nav/helse/flex/OppgaveBucket.kt
@@ -4,15 +4,19 @@ import com.google.api.gax.retrying.RetrySettings
 import com.google.cloud.storage.Blob
 import com.google.cloud.storage.BlobInfo
 import com.google.cloud.storage.StorageOptions
+import no.nav.helse.flex.client.SykepengesoknadBackendClient
+import no.nav.helse.flex.sykepengesoknad.kafka.SoknadsstatusDTO
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Component
 import org.threeten.bp.Duration
+import java.time.LocalDate
 import java.util.concurrent.TimeUnit
 
 @Component
 class OppgaveBucket(
     @Value("\${BUCKET_NAME}") private val bucketName: String,
+    private val sykepengesoknadBackendClient: SykepengesoknadBackendClient,
 ) {
 
     private val log = logger()
@@ -21,9 +25,9 @@ class OppgaveBucket(
 
     @Scheduled(initialDelay = 120, fixedDelay = 1_000, timeUnit = TimeUnit.SECONDS)
     fun job() {
-        val blobId = "test.csv"
+        val inputBlob = "test.csv"
 
-        val blob = getBlob(blobId)
+        val blob = getBlob(inputBlob)
 
         readFile(blob)
     }
@@ -33,11 +37,24 @@ class OppgaveBucket(
 
         log.info(content)
 
-        content.lines().forEachIndexed { lidx, line ->
-            log.info("linje $lidx = $line")
-            line.split(',').forEachIndexed { ridx, row ->
-                log.info("linje $lidx row $ridx = $row")
-            }
+        content.lines().forEach { line ->
+            val columns = line.split(',')
+
+            val fnr = columns[0]
+            val id = columns[1]
+            val fom = LocalDate.parse(columns[2])
+            val tom = LocalDate.parse(columns[3])
+            val cics = columns[4]
+
+            val soknad = sykepengesoknadBackendClient.hentSoknad(id)
+
+            log.info("Hentet soknad $soknad med cics $cics")
+
+            assert(soknad.fnr == fnr)
+            assert(soknad.id == id)
+            assert(soknad.fom == fom)
+            assert(soknad.tom == tom)
+            assert(soknad.status == SoknadsstatusDTO.SENDT)
         }
     }
 
