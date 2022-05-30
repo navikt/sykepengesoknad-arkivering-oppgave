6088ace227e8a6b8b45ffcba627dbf18c678a182
kulsveen <bjorn.inge.kulsveen@nav.no>
2022-05-30 13:45:05 +0200

    newFile.csv

--- src/main/kotlin/no/nav/helse/flex/OppgaveBucket.kt
+++ src/main/kotlin/no/nav/helse/flex/OppgaveBucket.kt
@@ -1,5 +1,6 @@
 package no.nav.helse.flex
 
+import com.google.api.gax.retrying.RetrySettings
 import com.google.cloud.storage.Blob
 import com.google.cloud.storage.BlobId
 import com.google.cloud.storage.BlobInfo
@@ -7,6 +8,7 @@ import com.google.cloud.storage.StorageOptions
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Component
+import org.threeten.bp.Duration
 import java.util.concurrent.TimeUnit
 
 @Component
@@ -15,18 +17,21 @@ class OppgaveBucket(
 ) {
 
     private val log = logger()
-    private val storage = StorageOptions.newBuilder().build().service
+    private val retrySettings = RetrySettings.newBuilder().setTotalTimeout(Duration.ofMillis(3000)).build()
+    private val storage = StorageOptions.newBuilder().setRetrySettings(retrySettings).build().service
 
     @Scheduled(initialDelay = 120, fixedDelay = 1_000, timeUnit = TimeUnit.SECONDS)
     fun job() {
-        val blob = getBlob("unik.csv")
-
-        log.info("Hentet blob ${blob.metadata}")
+        val blobId = "newFile.csv"
 
         createBlob(
-            blobId = "newFile.csv",
+            blobId = blobId,
             file = "abc, oiegjr"
         )
+
+        val blob = getBlob(blobId)
+
+        log.info("Hentet blob ${blob.metadata} ${blob.bucket} ${blob.blobId} $blob")
     }
 
     private fun getBlob(
@@ -43,7 +48,7 @@ class OppgaveBucket(
         val bId = BlobId.of(bucketName, blobId)
 
         val bInfo = BlobInfo.newBuilder(bId)
-            .setMetadata(mapOf("content-type" to "text/csv"))
+            .setContentType("text/csv")
             .build()
 
         storage.create(bInfo, file.encodeToByteArray())
