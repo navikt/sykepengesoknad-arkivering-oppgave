2927ad5b8ea2ce81b98566754067343ed2c314b3
kulsveen <bjorn.inge.kulsveen@nav.no>
2022-05-30 13:12:41 +0200

    Laster opp csv

--- src/main/kotlin/no/nav/helse/flex/OppgaveBucket.kt
+++ src/main/kotlin/no/nav/helse/flex/OppgaveBucket.kt
@@ -1,5 +1,8 @@
 package no.nav.helse.flex
 
+import com.google.cloud.storage.Blob
+import com.google.cloud.storage.BlobId
+import com.google.cloud.storage.BlobInfo
 import com.google.cloud.storage.Storage
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.scheduling.annotation.Scheduled
@@ -14,9 +17,33 @@ class OppgaveBucket(
 
     @Scheduled(initialDelay = 120, fixedDelay = 1_000, timeUnit = TimeUnit.SECONDS)
     fun job() {
-        val bucket = storage.get(bucketName) ?: throw RuntimeException("Fant ikke bøtte ved navn $bucketName")
-        val blob = bucket.get("unik.csv")
+        val blob = getBlob("unik.csv")
 
         log.info("Hentet blob ${blob.metadata}")
+
+        createBlob(
+            blobId = "newFile.csv",
+            file = "abc, oiegjr"
+        )
+    }
+
+    private fun getBlob(
+        blobId: String
+    ): Blob {
+        val bucket = storage.get(bucketName) ?: throw RuntimeException("Fant ikke bøtte ved navn $bucketName")
+        return bucket.get(blobId)
+    }
+
+    private fun createBlob(
+        blobId: String,
+        file: String,
+    ) {
+        val bId = BlobId.of(bucketName, blobId)
+
+        val bInfo = BlobInfo.newBuilder(bId)
+            .setMetadata(mapOf("content-type" to "text/csv"))
+            .build()
+
+        storage.create(bInfo, file.encodeToByteArray())
     }
 }
