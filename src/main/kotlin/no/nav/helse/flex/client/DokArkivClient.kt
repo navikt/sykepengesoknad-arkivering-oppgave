69aab3370d197b6ff792cfb7ec5eef4558d8ef7b
Håvard Stigen Andersen <havard.stigen.andersen@nav.no>
2022-07-15 14:51:29 +0200

    Håndter 409 conflict

--- src/main/kotlin/no/nav/helse/flex/client/DokArkivClient.kt
+++ src/main/kotlin/no/nav/helse/flex/client/DokArkivClient.kt
@@ -1,7 +1,9 @@
 package no.nav.helse.flex.client
 
+import com.fasterxml.jackson.module.kotlin.readValue
 import no.nav.helse.flex.domain.JournalpostRequest
 import no.nav.helse.flex.domain.JournalpostResponse
+import no.nav.helse.flex.util.OBJECT_MAPPER
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.http.HttpEntity
 import org.springframework.http.HttpHeaders
@@ -10,6 +12,7 @@ import org.springframework.http.MediaType
 import org.springframework.retry.annotation.Backoff
 import org.springframework.retry.annotation.Retryable
 import org.springframework.stereotype.Component
+import org.springframework.web.client.HttpClientErrorException
 import org.springframework.web.client.RestTemplate
 
 @Component
@@ -21,21 +24,25 @@ class DokArkivClient(
 
     @Retryable(backoff = Backoff(delay = 5000))
     fun opprettJournalpost(pdfRequest: JournalpostRequest, id: String): JournalpostResponse {
-        val url = "$dokarkivUrl/rest/journalpostapi/v1/journalpost?forsoekFerdigstill=true"
+        try {
+            val url = "$dokarkivUrl/rest/journalpostapi/v1/journalpost?forsoekFerdigstill=true"
 
-        val headers = HttpHeaders()
-        headers.contentType = MediaType.APPLICATION_JSON
-        headers["Nav-Callid"] = id
+            val headers = HttpHeaders()
+            headers.contentType = MediaType.APPLICATION_JSON
+            headers["Nav-Callid"] = id
 
-        val entity = HttpEntity(pdfRequest, headers)
+            val entity = HttpEntity(pdfRequest, headers)
 
-        val result = dokArkivRestTemplate.exchange(url, HttpMethod.POST, entity, JournalpostResponse::class.java)
+            val result = dokArkivRestTemplate.exchange(url, HttpMethod.POST, entity, JournalpostResponse::class.java)
 
-        if (!result.statusCode.is2xxSuccessful) {
-            throw RuntimeException("dokarkiv feiler med HTTP-${result.statusCode} for søknad med id: $id")
+            if (!result.statusCode.is2xxSuccessful) {
+                throw RuntimeException("dokarkiv feiler med HTTP-${result.statusCode} for søknad med id: $id")
+            }
+
+            return result.body
+                ?: throw RuntimeException("dokarkiv returnerer ikke data for søknad med id: $id")
+        } catch (e: HttpClientErrorException.Conflict) {
+            return OBJECT_MAPPER.readValue(e.responseBodyAsString)
         }
-
-        return result.body
-            ?: throw RuntimeException("dokarkiv returnerer ikke data for søknad med id: $id")
     }
 }
