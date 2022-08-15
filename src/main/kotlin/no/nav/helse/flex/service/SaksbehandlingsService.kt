e6ce266159171d6b37eb05c1316326709e87f3f4
Bjørn Inge Kulsveen <bjorn.inge.kulsveen@nav.no>
2022-08-15 09:44:01 +0200

    Debug av ruting for utland opphold søknader som tildeles feil enhet

--- src/main/kotlin/no/nav/helse/flex/service/SaksbehandlingsService.kt
+++ src/main/kotlin/no/nav/helse/flex/service/SaksbehandlingsService.kt
@@ -62,12 +62,33 @@ class SaksbehandlingsService(
             harRedusertVenteperiode = sykepengesoknad.harRedusertVenteperiode,
             speilRelatert = speilRelatert,
         )
-        val oppgaveId = oppgaveService.opprettOppgave(requestBody).id.toString()
+        val oppgaveResponse = oppgaveService.opprettOppgave(requestBody)
 
-        innsendingRepository.updateOppgaveId(id = innsending.id!!, oppgaveId = oppgaveId)
+        innsendingRepository.updateOppgaveId(id = innsending.id!!, oppgaveId = oppgaveResponse.id.toString())
 
         tellInnsendingBehandlet(soknad.soknadstype)
         log.info("Oppretter oppgave ${innsending.id} for ${soknad.soknadstype.name.lowercase()} søknad: ${soknad.soknadsId}")
+
+        if (soknad.soknadstype == Soknadstype.OPPHOLD_UTLAND) {
+            sjekkOmOppholdUtlandSendesTilEnhet4488(soknad, requestBody, oppgaveResponse)
+        }
+    }
+
+    private fun sjekkOmOppholdUtlandSendesTilEnhet4488(
+        soknad: Soknad,
+        req: OppgaveRequest,
+        res: OppgaveResponse,
+    ) {
+        if (res.tildeltEnhetsnr == "4488") {
+            log.warn(
+                "Søknad om opphold utland ${soknad.soknadsId} ble tildelt enhet ${res.tildeltEnhetsnr}. Request = ${
+                    req.copy(
+                        aktoerId = "***",
+                        beskrivelse = "***"
+                    )
+                }. Response $res"
+            )
+        }
     }
 
     fun settFerdigbehandlet(innsendingsId: String) {
