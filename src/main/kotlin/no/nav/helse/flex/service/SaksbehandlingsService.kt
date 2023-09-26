6a0cb36d9c6648036c5845ba340f6bf97e846872
Håvard Stigen Andersen <havard.stigen.andersen@nav.no>
2023-09-26 14:01:42 +0200

    Håndter manglende bilde (#269)

--- src/main/kotlin/no/nav/helse/flex/service/SaksbehandlingsService.kt
+++ src/main/kotlin/no/nav/helse/flex/service/SaksbehandlingsService.kt
@@ -13,6 +13,7 @@ import no.nav.helse.flex.logger
 import no.nav.helse.flex.repository.InnsendingDbRecord
 import no.nav.helse.flex.repository.InnsendingRepository
 import org.springframework.stereotype.Component
+import org.springframework.web.client.HttpClientErrorException
 import java.util.*
 
 @Component
@@ -50,7 +51,11 @@ class SaksbehandlingsService(
         return innsendingId!!
     }
 
-    fun opprettOppgave(sykepengesoknad: Sykepengesoknad, innsending: InnsendingDbRecord, speilRelatert: Boolean = false) {
+    fun opprettOppgave(
+        sykepengesoknad: Sykepengesoknad,
+        innsending: InnsendingDbRecord,
+        speilRelatert: Boolean = false
+    ) {
         val fnr = identService.hentFnrForAktorId(sykepengesoknad.aktorId)
 
         val soknad = opprettSoknad(sykepengesoknad, fnr)
@@ -120,14 +125,24 @@ class SaksbehandlingsService(
         val navn = pdlClient.hentFormattertNavn(fnr)
 
         val soknad = Soknad.lagSoknad(sykepengesoknad, fnr, navn)
-
+        fun PdfKvittering.hentOgSettKvittering(): PdfKvittering {
+            return try {
+                this.copy(
+                    b64data = Base64.getEncoder()
+                        .encodeToString(sykepengesoknadKvitteringerClient.hentVedlegg(this.blobId))
+                )
+            } catch (e: HttpClientErrorException.NotFound) {
+                if (sykepengesoknad.id == "3e7c6d64-8e24-3f0b-8e6e-b3211315789b") {
+                    log.info("Bruker blankt bilde for korrigert kvittering i soknad ${sykepengesoknad.id}")
+                    this.copy(
+                        b64data = "/9j/4AAQSkZJRgABAQEAYABgAAD/2wBDAAgGBgcGBQgHBwcJCQgKDBQNDAsLDBkSEw8UHRofHh0aHBwgJC4nICIsIxwcKDcpLDAxNDQ0Hyc5PTgyPC4zNDL/2wBDAQkJCQwLDBgNDRgyIRwhMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjL/wAARCAABAAEDASIAAhEBAxEB/8QAHwAAAQUBAQEBAQEAAAAAAAAAAAECAwQFBgcICQoL/8QAtRAAAgEDAwIEAwUFBAQAAAF9AQIDAAQRBRIhMUEGE1FhByJxFDKBkaEII0KxwRVS0fAkM2JyggkKFhcYGRolJicoKSo0NTY3ODk6Q0RFRkdISUpTVFVWV1hZWmNkZWZnaGlqc3R1dnd4eXqDhIWGh4iJipKTlJWWl5iZmqKjpKWmp6ipqrKztLW2t7i5usLDxMXGx8jJytLT1NXW19jZ2uHi4+Tl5ufo6erx8vP09fb3+Pn6/8QAHwEAAwEBAQEBAQEBAQAAAAAAAAECAwQFBgcICQoL/8QAtREAAgECBAQDBAcFBAQAAQJ3AAECAxEEBSExBhJBUQdhcRMiMoEIFEKRobHBCSMzUvAVYnLRChYkNOEl8RcYGRomJygpKjU2Nzg5OkNERUZHSElKU1RVVldYWVpjZGVmZ2hpanN0dXZ3eHl6goOEhYaHiImKkpOUlZaXmJmaoqOkpaanqKmqsrO0tba3uLm6wsPExcbHyMnK0tPU1dbX2Nna4uPk5ebn6Onq8vP09fb3+Pn6/9oADAMBAAIRAxEAPwD3+iiigD//2Q=="
+                    )
+                } else {
+                    throw e
+                }
+            }
+        }
         return soknad.copy(kvitteringer = soknad.kvitteringer?.map { it.hentOgSettKvittering() })
-    }
-
-    private fun PdfKvittering.hentOgSettKvittering(): PdfKvittering {
-        return this.copy(
-            b64data = Base64.getEncoder().encodeToString(sykepengesoknadKvitteringerClient.hentVedlegg(this.blobId))
-        )
     }
 
     private fun tellInnsendingBehandlet(soknadstype: Soknadstype?) {
