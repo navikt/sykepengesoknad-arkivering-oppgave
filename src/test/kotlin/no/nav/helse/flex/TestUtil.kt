4c2c3b8a44f215787db8da2842adf7361525cb10
Bjørn Inge Kulsveen <35369790+kulsveen@users.noreply.github.com>
2023-10-13 11:34:37 +0200

    Flytter dokument tittel test inn i integrasjonstest (#291)
    * Flytter dokument tittel test inn i integrasjonstest
    
    * Fjerner deprecated ConsumerRecord

--- src/test/kotlin/no/nav/helse/flex/TestUtil.kt
+++ src/test/kotlin/no/nav/helse/flex/TestUtil.kt
@@ -2,9 +2,6 @@ package no.nav.helse.flex
 
 import no.nav.helse.flex.sykepengesoknad.kafka.SykepengesoknadDTO
 import org.apache.kafka.clients.consumer.ConsumerRecord
-import org.apache.kafka.common.header.Headers
-import org.apache.kafka.common.header.internals.RecordHeaders
-import org.apache.kafka.common.record.TimestampType
 import org.mockito.Mockito
 
 /**
@@ -13,20 +10,13 @@ import org.mockito.Mockito
  */
 fun <T> any(): T = Mockito.any()
 
-fun <T> skapConsumerRecord(key: String, value: T, headers: Headers = RecordHeaders()): ConsumerRecord<String, T> {
-    @Suppress("DEPRECATION")
+fun <T> skapConsumerRecord(key: String, value: T): ConsumerRecord<String, T> {
     return ConsumerRecord(
         "topic-v1",
-        0,
-        0,
-        0,
-        TimestampType.CREATE_TIME,
-        0,
-        0,
-        0,
+        1,
+        1L,
         key,
-        value,
-        headers
+        value
     )
 }
 
