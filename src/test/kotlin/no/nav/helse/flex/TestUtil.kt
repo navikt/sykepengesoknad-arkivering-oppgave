cf8a89091d0fe2bd8489c3a28f1fee225ed05354
oivindkulsrud <oivindkulsrud@gmail.com>
2023-10-17 13:23:23 +0200

    har fått inn dto, ser ut til å la seg lese

--- src/test/kotlin/no/nav/helse/flex/TestUtil.kt
+++ src/test/kotlin/no/nav/helse/flex/TestUtil.kt
@@ -31,3 +31,10 @@ val mockReisetilskuddDTO: SykepengesoknadDTO =
         Application::class.java.getResource("/reisetilskuddAlleSvar.json"),
         SykepengesoknadDTO::class.java
     )
+
+
+val mockBehandlingsdagerdDTO: SykepengesoknadDTO =
+    objectMapper.readValue(
+        Application::class.java.getResource("/soknadBehandlingsdagerTodagerDTO.json"),
+        SykepengesoknadDTO::class.java
+    )
