8fb679ffb252942cc1c362df6cac46b20c096282
Håvard Stigen Andersen <havard.stigen.andersen@gmail.com>
2024-06-03 12:52:57 +0200

    Patch oppgaver som er feilregistrert

--- src/main/kotlin/no/nav/helse/flex/egenemeldinggate/FinnBerorteOppgaver.kt
+++ src/main/kotlin/no/nav/helse/flex/egenemeldinggate/FinnBerorteOppgaver.kt
@@ -1,16 +1,18 @@
 package no.nav.helse.flex.egenemeldinggate
 
 import no.nav.helse.flex.logger
+import no.nav.helse.flex.repository.InnsendingRepository
+import no.nav.helse.flex.repository.OppgaveStatus
 import no.nav.helse.flex.repository.SpreOppgaveRepository
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Component
 import java.time.*
 import java.util.concurrent.TimeUnit
-import kotlin.time.measureTime
 
 @Component
 class FinnBerorteOppgaver(
     private val spreOppgaveRepository: SpreOppgaveRepository,
+    private val innsendingRepository: InnsendingRepository,
 ) {
     private val log = logger()
 
@@ -21,15 +23,42 @@ class FinnBerorteOppgaver(
     }
 
     fun behandleOppgaver() {
-        val measureTime =
-            measureTime {
-                val oppgaver =
-                    spreOppgaveRepository.finnBerorteOppgaver(
-                        modifisertEtter = OffsetDateTime.of(2024, 6, 2, 7, 55, 0, 0, ZoneOffset.UTC).toInstant(),
-                        opprettetEtter = OffsetDateTime.of(2024, 5, 31, 7, 0, 0, 0, ZoneOffset.UTC).toInstant(),
-                    )
-                log.info("Fant ${oppgaver.size} berørte oppgave av egenmeldinggate")
+        val oppgaver =
+            spreOppgaveRepository.finnBerorteOppgaver(
+                modifisertEtter = OffsetDateTime.of(2024, 6, 2, 7, 55, 0, 0, ZoneOffset.UTC).toInstant(),
+                opprettetEtter = OffsetDateTime.of(2024, 5, 31, 7, 0, 0, 0, ZoneOffset.UTC).toInstant(),
+            )
+        log.info("Fant ${oppgaver.size} berørte oppgave av egenmeldinggate")
+
+        if (oppgaver.size == 6639) {
+            oppgaver.forEachIndexed { index, spreOppgaveDbRecord ->
+                val innsending =
+                    innsendingRepository.findBySykepengesoknadId(spreOppgaveDbRecord.sykepengesoknadId)
+                        ?: throw RuntimeException("Fant ikke innsending for sykepengesoknadId ${spreOppgaveDbRecord.sykepengesoknadId}")
+
+                log.info(
+                    "Fjerner oppgave id ${innsending.oppgaveId} fra innsending ${innsending.id} " +
+                        "med sykepengesoknadId ${spreOppgaveDbRecord.sykepengesoknadId}",
+                )
+                innsendingRepository.save(innsending.copy(oppgaveId = null))
+
+                log.info(
+                    "Oppdaterer status til UTSETT for oppgavestyring ${spreOppgaveDbRecord.id} med " +
+                        "sykepengesoknadId ${spreOppgaveDbRecord.sykepengesoknadId}",
+                )
+                spreOppgaveRepository.save(
+                    spreOppgaveDbRecord.copy(
+                        status = OppgaveStatus.Utsett,
+                        timeout =
+                            OffsetDateTime.of(2024, 6, 6, 3, 0, 0, 0, ZoneOffset.UTC)
+                                .toInstant(),
+                        modifisert = Instant.now(),
+                    ),
+                )
+                log.info("Ferdig med nr $index av ${oppgaver.size} oppgaver. ID: ${spreOppgaveDbRecord.id}")
             }
-        log.info("Hentet oppgaver tok $measureTime")
+        } else {
+            log.error("Fant feil antall oppgaver, forventet 6639 men fant ${oppgaver.size}")
+        }
     }
 }
