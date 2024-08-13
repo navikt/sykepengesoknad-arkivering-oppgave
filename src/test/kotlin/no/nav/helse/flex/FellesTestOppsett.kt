0226f2f69715b1278bdc2f23d55b738c3383210e
Nils Jørgen Mittet <88632815+nilsjorgen@users.noreply.github.com>
2024-08-13 12:56:02 +0200

    Bump til Kotlin 2.0.10 (#463)

--- src/test/kotlin/no/nav/helse/flex/FellesTestOppsett.kt
+++ src/test/kotlin/no/nav/helse/flex/FellesTestOppsett.kt
@@ -33,7 +33,6 @@ import org.testcontainers.containers.KafkaContainer
 import org.testcontainers.containers.PostgreSQLContainer
 import org.testcontainers.utility.DockerImageName
 import java.util.concurrent.TimeUnit
-import kotlin.concurrent.thread
 
 private class PostgreSQLContainer14 : PostgreSQLContainer<PostgreSQLContainer14>("postgres:14-alpine")
 
@@ -42,78 +41,62 @@ private class PostgreSQLContainer14 : PostgreSQLContainer<PostgreSQLContainer14>
 @EnableMockOAuth2Server
 abstract class FellesTestOppsett {
     companion object {
-        val pdlMockWebserver: MockWebServer
-        val pdfMockWebserver: MockWebServer
-        val dokArkivMockWebserver: MockWebServer
-        val oppgaveMockWebserver: MockWebServer
-        val sykepengesoknadMockWebserver: MockWebServer
-        val kvitteringMockWebserver: MockWebServer
-        val medlemskapMockWebserver: MockWebServer
-
         init {
-            val threads = mutableListOf<Thread>()
+            PostgreSQLContainer14().apply {
+                withCommand("postgres", "-c", "wal_level=logical")
+                start()
+                System.setProperty("spring.datasource.url", "$jdbcUrl&reWriteBatchedInserts=true")
+                System.setProperty("spring.datasource.username", username)
+                System.setProperty("spring.datasource.password", password)
+            }
 
-            thread {
-                PostgreSQLContainer14().apply {
-                    withCommand("postgres", "-c", "wal_level=logical")
-                    start()
-                    System.setProperty("spring.datasource.url", "$jdbcUrl&reWriteBatchedInserts=true")
-                    System.setProperty("spring.datasource.username", username)
-                    System.setProperty("spring.datasource.password", password)
-                }
-            }.also { threads.add(it) }
-
-            thread {
-                KafkaContainer(DockerImageName.parse("confluentinc/cp-kafka:7.6.1")).apply {
-                    start()
-                    System.setProperty("KAFKA_BROKERS", bootstrapServers)
-                }
-            }.also { threads.add(it) }
-
-            pdlMockWebserver =
-                MockWebServer().apply {
-                    System.setProperty("PDL_URL", "http://localhost:$port")
-                    dispatcher = PdlMockDispatcher
-                }
-
-            pdfMockWebserver =
-                MockWebServer().apply {
-                    System.setProperty("PDFGEN_URL", "http://localhost:$port")
-                    dispatcher = PdfMockDispatcher
-                }
-
-            dokArkivMockWebserver =
-                MockWebServer().apply {
-                    System.setProperty("DOKARKIV_URL", "http://localhost:$port")
-                    dispatcher = DokArkivMockDispatcher
-                }
-
-            oppgaveMockWebserver =
-                MockWebServer().apply {
-                    System.setProperty("OPPGAVE_URL", "http://localhost:$port")
-                    dispatcher = OppgaveMockDispatcher
-                }
-
-            sykepengesoknadMockWebserver =
-                MockWebServer().apply {
-                    System.setProperty("SYKEPENGESOKNAD_BACKEND_URL", "http://localhost:$port")
-                    dispatcher = SykepengesoknadMockDispatcher
-                }
-
-            kvitteringMockWebserver =
-                MockWebServer().apply {
-                    System.setProperty("SYKEPENGESOKNAD_KVITTERINGER_URL", "http://localhost:$port")
-                    dispatcher = KvitteringMockDispatcher
-                }
-
-            medlemskapMockWebserver =
-                MockWebServer().apply {
-                    System.setProperty("MEDLEMSKAP_VURDERING_URL", "http://localhost:$port")
-                    dispatcher = MedlemskapMockDispatcher
-                }
-
-            threads.forEach { it.join() }
+            KafkaContainer(DockerImageName.parse("confluentinc/cp-kafka:7.6.1")).apply {
+                start()
+                System.setProperty("KAFKA_BROKERS", bootstrapServers)
+            }
         }
+
+        val pdlMockWebserver =
+            MockWebServer().apply {
+                System.setProperty("PDL_URL", "http://localhost:$port")
+                dispatcher = PdlMockDispatcher
+            }
+
+        val pdfMockWebserver =
+            MockWebServer().apply {
+                System.setProperty("PDFGEN_URL", "http://localhost:$port")
+                dispatcher = PdfMockDispatcher
+            }
+
+        val dokArkivMockWebserver =
+            MockWebServer().apply {
+                System.setProperty("DOKARKIV_URL", "http://localhost:$port")
+                dispatcher = DokArkivMockDispatcher
+            }
+
+        val oppgaveMockWebserver =
+            MockWebServer().apply {
+                System.setProperty("OPPGAVE_URL", "http://localhost:$port")
+                dispatcher = OppgaveMockDispatcher
+            }
+
+        val sykepengesoknadMockWebserver =
+            MockWebServer().apply {
+                System.setProperty("SYKEPENGESOKNAD_BACKEND_URL", "http://localhost:$port")
+                dispatcher = SykepengesoknadMockDispatcher
+            }
+
+        val kvitteringMockWebserver =
+            MockWebServer().apply {
+                System.setProperty("SYKEPENGESOKNAD_KVITTERINGER_URL", "http://localhost:$port")
+                dispatcher = KvitteringMockDispatcher
+            }
+
+        val medlemskapMockWebserver =
+            MockWebServer().apply {
+                System.setProperty("MEDLEMSKAP_VURDERING_URL", "http://localhost:$port")
+                dispatcher = MedlemskapMockDispatcher
+            }
     }
 
     @Autowired
@@ -166,13 +149,20 @@ abstract class FellesTestOppsett {
 
     @AfterAll
     fun hentMockRequests() {
-        while (pdlMockWebserver.takeRequest(1, TimeUnit.MILLISECONDS) != null) { /* ok */ }
-        while (pdfMockWebserver.takeRequest(1, TimeUnit.MILLISECONDS) != null) { /* ok */ }
-        while (dokArkivMockWebserver.takeRequest(1, TimeUnit.MILLISECONDS) != null) { /* ok */ }
-        while (oppgaveMockWebserver.takeRequest(1, TimeUnit.MILLISECONDS) != null) { /* ok */ }
-        while (sykepengesoknadMockWebserver.takeRequest(1, TimeUnit.MILLISECONDS) != null) { /* ok */ }
-        while (kvitteringMockWebserver.takeRequest(1, TimeUnit.MILLISECONDS) != null) { /* ok */ }
-        while (medlemskapMockWebserver.takeRequest(1, TimeUnit.MILLISECONDS) != null) { /* ok */ }
+        while (pdlMockWebserver.takeRequest(1, TimeUnit.MILLISECONDS) != null) { // ok
+        }
+        while (pdfMockWebserver.takeRequest(1, TimeUnit.MILLISECONDS) != null) { // ok
+        }
+        while (dokArkivMockWebserver.takeRequest(1, TimeUnit.MILLISECONDS) != null) { // ok
+        }
+        while (oppgaveMockWebserver.takeRequest(1, TimeUnit.MILLISECONDS) != null) { // ok
+        }
+        while (sykepengesoknadMockWebserver.takeRequest(1, TimeUnit.MILLISECONDS) != null) { // ok
+        }
+        while (kvitteringMockWebserver.takeRequest(1, TimeUnit.MILLISECONDS) != null) { // ok
+        }
+        while (medlemskapMockWebserver.takeRequest(1, TimeUnit.MILLISECONDS) != null) { // ok
+        }
     }
 
     fun leggSøknadPåKafka(søknad: SykepengesoknadDTO) =
