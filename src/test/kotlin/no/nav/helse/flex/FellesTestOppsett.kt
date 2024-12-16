5685080c503677e00f026f53daa13cb895b9111e
Nils Jørgen Mittet <88632815+nilsjorgen@users.noreply.github.com>
2024-12-16 15:56:39 +0100

    Pin kafka-native til 3.8.1 (#524)
    Se https://github.com/navikt/amt-lib/pull/84

--- src/test/kotlin/no/nav/helse/flex/FellesTestOppsett.kt
+++ src/test/kotlin/no/nav/helse/flex/FellesTestOppsett.kt
@@ -50,7 +50,7 @@ abstract class FellesTestOppsett {
                 System.setProperty("spring.datasource.password", password)
             }
 
-            KafkaContainer(DockerImageName.parse("apache/kafka-native")).apply {
+            KafkaContainer(DockerImageName.parse("apache/kafka-native:3.8.1")).apply {
                 start()
                 System.setProperty("KAFKA_BROKERS", bootstrapServers)
             }
