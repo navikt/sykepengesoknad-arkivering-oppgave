feb9d68f4d16e6f646215d84b770ade483ecf394
Nils Jørgen Mittet <nils.jorgen.mittet@nav.no>
2026-07-29 08:08:35 +0200

    Fjern bruk av SporsmalDTO.metadata

--- src/main/kotlin/no/nav/helse/flex/domain/dto/Sporsmal.kt
+++ src/main/kotlin/no/nav/helse/flex/domain/dto/Sporsmal.kt
@@ -1,9 +1,5 @@
 package no.nav.helse.flex.domain.dto
 
-import com.fasterxml.jackson.annotation.JsonProperty
-import com.fasterxml.jackson.databind.JsonNode
-import java.math.BigInteger
-
 data class Sporsmal(
     val id: String,
     val tag: String,
@@ -15,25 +11,4 @@ data class Sporsmal(
     val kriterieForVisningAvUndersporsmal: Visningskriterie? = null,
     val svar: List<Svar>? = null,
     val undersporsmal: List<Sporsmal>? = null,
-    var metadata: JsonNode? = null,
-)
-
-data class AarVerdi(
-    val aar: String,
-    val verdi: BigInteger,
-)
-
-data class Beregnet(
-    val snitt: BigInteger,
-    val p25: BigInteger,
-    val m25: BigInteger,
-)
-
-data class SigrunInntekt(
-    val inntekter: List<AarVerdi>,
-    @param:JsonProperty(value = "g-verdier")
-    val gVerdier: List<AarVerdi>,
-    @param:JsonProperty(value = "g-sykmelding")
-    val gSykmelding: BigInteger,
-    val beregnet: Beregnet,
 )
